488a79cc6d17c840064693e133da24ae4b4cb22b
Raj8802 <106430619+Raj8802@users.noreply.github.com>
2022-05-29 18:15:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -21,7 +21,7 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. Metro HD Moviez should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Metro HD Moviez
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
